56b1b4a363ddec387a87d5da61dab851f1fc04f8
junaidkhan046 <khanjunaid046@gmail.com>
2024-06-09 19:45:33 +0530

    add supplier registration

--- resources/views/auth/layout/logo.blade.php
+++ resources/views/auth/layout/logo.blade.php
@@ -1,6 +1,12 @@
-<div class="brand-logo d-flex justify-content-center">
-    <a href="#"><img src="{{asset('assets/images/Logo.svg')}}" alt="Logo" /></a>
+
     @if(Request::path() == 'supplier/login' || Request::path() == 'supplier/register')
-    <span class="sub-title">Seller Center</span>
+    <div class="brand-logo d-flex justify-content-center align-items-center">
+              <a href="#"><img src="{{asset('assets/images/Logo.svg')}}" alt="Logo" /></a>
+               <div class="SellerText">Seller Center</div>
+            </div>
+    @else
+    <div class="brand-logo d-flex justify-content-center">
+    <a href="#"><img src="{{asset('assets/images/Logo.svg')}}" alt="Logo" /></a>
+    </div>
     @endif
-</div>
+
