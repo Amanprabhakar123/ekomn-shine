ead86b705ded9a11a9a6d70425ceaeb7062d011d
JunaidKhan046 <32728702+JunaidKhan046@users.noreply.github.com>
2024-06-16 08:40:22 +0530

    validation update 3

--- resources/views/auth/buyer/register.blade.php
+++ resources/views/auth/buyer/register.blade.php
@@ -571,7 +571,7 @@ $(document).ready(function () {
 
     const businessNameRegex = /^[a-zA-Z0-9\s&.\-]+$/;
       const gstRegex = /^[0-9]{2}[A-Z]{5}[0-9]{4}[A-Z]{1}[1-9A-Z]{1}Z[0-9A-Z]{1}$/;
-      const nameRegex = /^[a-zA-Z\s\-']+$/;
+      const nameRegex = /^[a-zA-Z\s\-\.']+$/;
       const addressRegex = /^[a-zA-Z0-9\s,.'\-\/]+$/;
       const pinCodeRegex = /^[0-9]{6}$/;
       const mobileRegex = /^[6-9]\d{9}$/;
