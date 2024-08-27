5811ea0725a39932f398afe1b1ed763c55b0d2d3
JunaidKhan046 <imranraza00000@gmail.com>
2024-08-27 18:46:51 +0530

    admin update

---
+++ app/Http/Controllers/APIAuth/AdminController.php
@@ -0,0 +1,192 @@
+<?php
+
+namespace App\Http\Controllers\ApiAuth;
+
+use App\Models\User;
+use League\Fractal\Manager;
+use Illuminate\Http\Request;
+use App\Events\ExceptionEvent;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+use League\Fractal\Resource\Collection;
+use Spatie\Permission\Models\Permission;
+use App\Transformers\SubAdminTransformer;
+use Illuminate\Support\Facades\Validator;
+use League\Fractal\Pagination\IlluminatePaginatorAdapter;
+
+
+
+
+class AdminController extends Controller
+{
+
+    protected $fractal;
+
+    public function __construct(Manager $fractal)
+    {
+        $this->fractal = $fractal;
+    }
+     /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('dashboard.admin.admin_list');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+
+    public function addAdmin()
+    {
+        $permission = Permission::all();
+        $permissionsArray = $permission->toArray();
+
+        // dd($permissionsArray);
+        $permissions = [];
+
+        foreach ($permissionsArray as $key =>  $item) {
+            $permissions[$item['name']] = str_replace('_', ' ', ucfirst($item['name']));
+        }
+
+        // dd($permissions);
+        return view('dashboard.admin.create_admin', compact('permissions'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+
+    public function subAdminStore(Request $request)
+    {
+        try {
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string',
+                'email' => 'required|string|email|unique:users',
+                'permissions' => 'required|string',
+                'password' => 'required|string|min:8|regex:/^.*(?=.{3,})(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[\d\x])(?=.*[!$#%@_.]).*$/|confirmed',
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json(['data' => [
+                    'statusCode' => __('statusCode.statusCode400'),
+                    'status' => __('statusCode.status400'),
+                    'message' => $validator->errors()->first(),
+                ]], __('statusCode.statusCode400'));
+            }
+
+            // Create user
+            $user = User::create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            // Assign role to user
+            $user->assignRole(User::ROLE_SUB_ADMIN);
+
+            $permission =  explode(',', $request->permissions);
+            // Assign permission to user
+            $user->givePermissionTo($permission);
+
+            return response()->json(['data' => [
+                'statusCode' => __('statusCode.statusCode200'),
+                'status' => __('statusCode.status200'),
+                'message' => __('auth.registerSuccess'),
+            ]], __('statusCode.statusCode200'));
+        } catch (\Exception $e) {
+            $exceptionDetails = [
+                'message' => $e->getMessage(),
+                'file' => $e->getFile(),
+                'line' => $e->getLine(),
+            ];
+
+            // Trigger the event
+            event(new ExceptionEvent($exceptionDetails));
+            return response()->json(['data' => [
+                'statusCode' => __('statusCode.statusCode500'),
+                'status' => __('statusCode.status500'),
+                'message' => $e->getMessage(),
+            ]], __('statusCode.statusCode500'));
+        }
+    }
+
+    /**
+     * Display the admin list.
+     */
+    public function adminList(Request $request){
+       
+        try {
+            $perPage = $request->get('per_page', 10);
+
+            $searchTerm = $request->input('query', null);
+            // $sort_by_status = $request->input('sort_by_status'); // Default sort by 'all'
+            $users = User::role(User::ROLE_SUB_ADMIN);
+
+            // $users =  $users = User::role(User::ROLE_SUB_ADMIN)->get()->when($searchTerm, function ($query, $searchTerm) {
+            //     $query->where('name', 'like', '%' . $searchTerm . '%')
+            //         ->orWhere('email', 'like', '%' . $searchTerm . '%')
+            //         ->orWhereHas('companyDetails', function ($query) use ($searchTerm) {
+            //             $query->where('business_name', 'like', '%' . $searchTerm . '%')
+            //             ->orWhere('company_serial_id', 'like', '%' . $searchTerm . '%') // Search by company serial id
+            //                 ->orWhere('mobile_no', 'like', '%' . $searchTerm . '%');
+            //         });
+            // });
+
+            $users = $users->paginate($perPage);
+            
+
+            // Transform the paginated results using Fractal
+            $resource = new Collection($users, new SubAdminTransformer);
+
+            // Add pagination information to the resource
+            $resource->setPaginator(new IlluminatePaginatorAdapter($users));
+
+            // Create the data array using Fractal
+            $data = $this->fractal->createData($resource)->toArray();
+            // dd($data);
+            // Return the JSON response with paginated data
+            return response()->json($data);
+        } catch (\Exception $e) {
+            $exceptionDetails = [
+                'message' => $e->getMessage(),
+                'line' => $e->getLine(),
+                'file' => $e->getFile(),
+            ];
+
+            dd($exceptionDetails, $e->getMessage());
+            event(new ExceptionEvent($exceptionDetails));
+        }
+    }
+
+    public function updateUserActive(Request $request)
+    {
+        try {
+            $user = User::find(salt_decrypt($request->id));
+            $user->isactive = $request->status;
+            $user->save();
+            return response()->json([
+                'data' => [
+                    'statusCode' => __('statusCode.statusCode200'),
+                    'status' => __('statusCode.status200'),
+                    'message' => __('auth.userUpdated'),
+                ],
+            ], __('statusCode.statusCode200'));
+        } catch (\Exception $e) {
+            $exceptionDetails = [
+                'message' => $e->getMessage(),
+                'line' => $e->getLine(),
+                'file' => $e->getFile(),
+            ];
+            event(new ExceptionEvent($exceptionDetails));
+            return response()->json([
+                'data' => [
+                    'statusCode' => __('statusCode.statusCode422'),
+                    'status' => __('statusCode.status422'),
+                    'message' => $e->getMessage(),
+                ],
+            ], __('statusCode.statusCode422'));
+        }
+    }
+}
--- resources/views/dashboard/admin/admin_list.blade.php
+++ resources/views/dashboard/admin/admin_list.blade.php
@@ -54,16 +54,189 @@
 <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
 <script>
-    $('docyment').ready(function() {
-        ApiRequest('admin-list-get', 'GET')
-        .then(res => {
-            console.log(res.data);
-            if(res.data.statusCode == 200) {
-                let data = res.data.data;
-                let html = '';
-                data.forEach((item, index) => {
-                    html += `
-                        <tr>
+    document.addEventListener("DOMContentLoaded", () => {
+    
+        const rowsPerPage = document.getElementById("rowsPerPage");
+        const rowInfo = document.getElementById("rowInfo");
+        const pagination = document.getElementById("pagination");
+        const prevPage = document.getElementById("prevPage");
+        const nextPage = document.getElementById("nextPage");
+        const dataContainer = document.getElementById("dataContainer");
+        let currentPage = 1;
+        let rows = parseInt(rowsPerPage.value, 10);
+        let totalRows = 0;
+        // Event listener for the search input field
+        const searchQuery = document.getElementById("searchQuery");
+        searchQuery.addEventListener("keydown", (e) => {
+            if (e.key === "Enter") {
+                fetchData();
+            }
+        });
+
+        // Event listener for clicking outside the search input field
+        searchQuery.addEventListener("blur", (e) => {
+            fetchData();
+        });
+
+        // const sortByStatus = document.getElementById("sort_by_status");
+        // sortByStatus.addEventListener("change", () => {
+        //     fetchData();
+        // });
+
+        // let sortField = ""; // Set the sort field here (e.g. "sku", "stock", "selling_price")
+        // let sortOrder = ""; // Set the sort order here (e.g. "asc", "desc")
+        // const h_sorting = document.querySelectorAll(".h_sorting");
+        // h_sorting.forEach(element => {
+        //     element.addEventListener("click", () => {
+        //         const sortFieldElement = element;
+        //         sortField = sortFieldElement.getAttribute("data-sort-field");
+        //         sortOrder = (sortOrder === "asc") ? "desc" : "asc";
+        //         fetchData();
+        //         h_sorting.forEach(el => {
+        //             el.classList.remove("active");
+        //             el.classList.remove("asc");
+        //             el.classList.remove("desc");
+        //         });
+        //         element.classList.add("active");
+        //         element.classList.add(sortOrder);
+        //     });
+        // });
+        // Function to fetch data from the server
+        function fetchData() {
+            
+            // Make an API request to fetch inventory data
+            let apiUrl = `admin-list-get?per_page=${rows}&page=${currentPage}`;
+
+            // if (sortField && sortOrder) {
+            //     apiUrl += `&sort=${sortField}&order=${sortOrder}`;
+            // }
+
+            if (searchQuery) {
+                apiUrl += `&query=${searchQuery.value}`;
+            }
+
+            // if (sortByStatus) {
+            //     apiUrl += `&sort_by_status=${sortByStatus.value}`;
+            // }
+
+            ApiRequest(apiUrl, 'GET')
+                .then(response => {
+                    const data = (response.data);
+                    if (data.length === 0) {
+                        dataContainer.innerHTML = `<tr><td colspan="10" class="text-center">No data found</td></tr>`;
+                    } else {
+                        response = (response.meta.pagination);
+                        totalRows = response.total;
+                        updatePagination();
+                        displayData(data);
+                    }
+                })
+                .catch(error => {
+                    console.error('Error fetching data:', error);
+                });
+        }
+
+        // Function to update the pagination UI
+        function updatePagination() {
+            const totalPages = Math.ceil(totalRows / rows);
+            pagination.innerHTML = "";
+            let pageList = "";
+            if (totalPages <= 5) {
+                for (let i = 1; i <= totalPages; i++) {
+                    pageList += `<li><a href="#" class="${i === currentPage ? "active" : ""}" data-page="${i}">${i}</a></li>`;
+                }
+            } else {
+                if (currentPage <= 3) {
+                    for (let i = 1; i <= 4; i++) {
+                        pageList += `<li><a href="#" class="${i === currentPage ? "active" : ""}" data-page="${i}">${i}</a></li>`;
+                    }
+                    pageList += `<li>...</li>`;
+                    pageList += `<li><a href="#" data-page="${totalPages}">${totalPages}</a></li>`;
+                } else if (currentPage >= totalPages - 2) {
+                    pageList += `<li><a href="#" data-page="1">1</a></li>`;
+                    pageList += `<li>...</li>`;
+                    for (let i = totalPages - 3; i <= totalPages; i++) {
+                        pageList += `<li><a href="#" class="${i === currentPage ? "active" : ""}" data-page="${i}">${i}</a></li>`;
+                    }
+                } else {
+                    pageList += `<li><a href="#" data-page="1">1</a></li>`;
+                    pageList += `<li>...</li>`;
+                    for (let i = currentPage - 1; i <= currentPage + 1; i++) {
+                        pageList += `<li><a href="#" class="${i === currentPage ? "active" : ""}" data-page="${i}">${i}</a></li>`;
+                    }
+                    pageList += `<li>...</li>`;
+                    pageList += `<li><a href="#" data-page="${totalPages}">${totalPages}</a></li>`;
+                }
+            }
+            pagination.innerHTML = pageList;
+            updateRowInfo();
+            prevPage.disabled = currentPage === 1;
+            nextPage.disabled = currentPage === totalPages;
+        }
+
+        // Function to update the row information
+        function updateRowInfo() {
+            const startRow = (currentPage - 1) * rows + 1;
+            const endRow = Math.min(currentPage * rows, totalRows);
+            rowInfo.textContent = `Showing ${startRow} to ${endRow} of ${totalRows}`;
+        }
+
+        // Function to display the inventory data in the table
+        function displayData(items) {
+            dataContainer.innerHTML = items.map(generateTableRow).join("");
+        }
+
+        // Event listener for the "rowsPerPage" select element
+        rowsPerPage.addEventListener("change", (e) => {
+            rows = parseInt(e.target.value, 10);
+            currentPage = 1;
+            fetchData();
+        });
+
+        // Event listener for the pagination links
+        pagination.addEventListener("click", (e) => {
+            if (e.target.tagName === "A") {
+                currentPage = parseInt(e.target.dataset.page, 10);
+                fetchData();
+            }
+        });
+
+        // Event listener for the "prevPage" button
+        prevPage.addEventListener("click", () => {
+            if (currentPage > 1) {
+                currentPage--;
+                fetchData();
+            }
+        });
+
+        // Event listener for the "nextPage" button
+        nextPage.addEventListener("click", () => {
+            const totalPages = Math.ceil(totalRows / rows);
+            if (currentPage < totalPages) {
+                currentPage++;
+                fetchData();
+            }
+        });
+
+        // Initial fetch of data
+        fetchData();
+
+    });
+
+    /**
+     * Generates a table row HTML markup for a given item.
+     *
+     * @param {Object} item - The item object containing the details.
+     * @returns {string} - The HTML markup for the table row.
+     */
+    function generateTableRow(item) {
+        // let stock = allowEditable(item);
+        let isDisabled = "";
+        if (item.order_action_status == 6) {
+            isDisabled = "disabled";
+        }
+        return `
+        <tr>
             <td>
                  <div class="productTitle_t">
                    ${item.name}
@@ -75,14 +248,107 @@
 
                 </div>
             </td>
-            
+             <td>
+                <select class="changeStatus_t form-select" ${isDisabled} onchange="handleInput('${item.id}', 2, this)">
+                   <option value="1" ${item.status == "1" ? "selected" : ""}>Active</option>
+                   <option value="2" ${item.status == "2   " ? "selected" : ""}>In Active</option>
+                </select>
+            </td>
         </tr>
-                    `;
-                });
-                $('#dataContainer').html(html);
+    `;
+    }
+
+    /**
+     * Copies the provided text to the clipboard.
+     *
+     * @param {string} text - The text to be copied.
+     */
+    function copyText(text) {
+        const textarea = document.createElement('textarea');
+        textarea.value = text;
+        document.body.appendChild(textarea);
+        textarea.select();
+        document.execCommand('copy');
+        document.body.removeChild(textarea);
+    }
+
+
+
+    /**
+     * Handles the input event for the status dropdown.
+     *
+     * @param {number} itemId - The ID of the item.
+     * @param {number} type - The type of the input.
+     * @param {HTMLElement} element - The input element.
+     */
+    function handleInput(itemId, type, element) {
+        updateStatus(itemId, element.value);
+    }
+
+
+    /**
+     * Updates the status of a product.
+     *
+     * @param {number} productId - The ID of the product.
+     */
+    function updateStatus(itemId, newStatus) {
+        Swal.fire({
+            title: "Do you want to update status active ina-active?",
+            showCancelButton: true,
+            confirmButtonText: "Save",
+            denyButtonText: `Don't save`,
+            didOpen: () => {
+                const title = Swal.getTitle();
+                title.style.fontSize = '25px';
+                // Apply inline CSS to the content
+                const content = Swal.getHtmlContainer();
+                // Apply inline CSS to the confirm button
+                const confirmButton = Swal.getConfirmButton();
+                confirmButton.style.backgroundColor = '#feca40';
+                confirmButton.style.color = 'white';
+            }
+        }).then((result) => {
+            /* Read more about isConfirmed, isDenied below */
+            if (result.isConfirmed) {
+                // Make an API request to update the status of the product
+                ApiRequest(`update-admin-active`, 'POST', {
+                        status: newStatus,
+                        'id': itemId
+                    })
+                    .then(response => {
+                        Swal.fire({
+                            title: "Good job!",
+                            text: response.data.message,
+                            icon: "success",
+                            didOpen: () => {
+                                // Apply inline CSS to the title
+                                const title = Swal.getTitle();
+                                title.style.color = 'red';
+                                title.style.fontSize = '20px';
+
+                                // Apply inline CSS to the content
+                                const content = Swal.getHtmlContainer();
+
+                                // Apply inline CSS to the confirm button
+                                const confirmButton = Swal.getConfirmButton();
+                                confirmButton.style.backgroundColor = '#feca40';
+                                confirmButton.style.color = 'white';
+                            }
+                        }).then(() => {
+                            // Redirect to the inventory page
+                            window.location.href = "{{ route('admin.list') }}";
+                        })
+                    })
+                    .catch(error => {
+                        console.error('Error updating status:', error);
+                    });
+
+            } else if (result.isDenied) {
+                Swal.fire("The status is not updated.", "", "info");
             }
         });
-    });
+
+    }
 </script>
 
 @endsection
